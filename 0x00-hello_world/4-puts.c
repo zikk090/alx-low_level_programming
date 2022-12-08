4e1936c5dac8f5c7f7bdad9b1e26026ca8fcd0c4
Ezinne <ekalu.fk@gmail.com>
2022-12-08 10:26:36 -0800

    fixing task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
+	/*THis is a betty comment*/
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 	puts(myStr);
 
 	return (0);
