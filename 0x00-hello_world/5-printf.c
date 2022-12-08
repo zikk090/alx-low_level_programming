694eac8ee8c4cdd7d32a9f30c8a7e5322220b7af
Ezinne <ekalu.fk@gmail.com>
2022-12-08 11:39:59 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * This is a comment*/
 int main(void) 
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	
 	return 0;
 }
